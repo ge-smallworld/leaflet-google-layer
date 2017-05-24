bad739fdc4e499aca7224acf8654ebaba7278c4a
Enrique Cano <enrique.canocarballar@ge.com>
2017-05-24 15:55:52 +0100

    Fix defect, add uglified and browserified versions

--- leaflet-google-layer.js
+++ leaflet-google-layer.js
@@ -1,4 +1,6 @@
-var L = require('leaflet');
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
+var L = (typeof window !== "undefined" ? window['L'] : typeof global !== "undefined" ? global['L'] : null);
 
 var VALID_MAP_TYPES = ['roadmap', 'satellite'];
 
@@ -209,3 +211,6 @@ L.tileLayer.google = function (options) {
 };
 
 module.exports = L.TileLayer.Google;
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}]},{},[1]);
